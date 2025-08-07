5cabc35cf2b7b4cc90edc09fc564f8253ed52561
Oakho-Z <zhangtong0630@outlook.com>
2025-08-07 20:48:44 +1000

    Update app.py

--- app.py
+++ app.py
@@ -153,6 +153,10 @@ def chart_data():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
+import os
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5053, debug=True)
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5053))  # Railway用PORT，默认本地5053
+    app.run(host='0.0.0.0', port=port, debug=True)
+
+
